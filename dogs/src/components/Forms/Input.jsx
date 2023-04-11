e53b649682b95185eaf42e5f78b054e0dee7b775
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2023-04-11 20:20:24 -0300

    feat: adição de novos endpoints no api.js e useForm

--- dogs/src/components/Forms/Input.jsx
+++ dogs/src/components/Forms/Input.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  onBlur,
+  validate,
+  error,
+}) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} className={styles.input} type={type} name={name}></input>;
-      <p className={styles.error}>Error</p>
+      <input
+        id={name}
+        className={styles.input}
+        type={type}
+        name={name}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
--- dogs/src/components/login/LoginForm.jsx
+++ dogs/src/components/login/LoginForm.jsx
@@ -2,35 +2,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../api";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
 
-  function handleSubmit(event) {
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json)
+  }
+ 
+  async function handleSubmit(event) {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
+
+    if (username.validate() && password.validate()) {
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
       });
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="usuário" type="text" name="username" />
-        <Input label="senha" type="password" name="password" />
+        <Input label="usuário" type="text" name="username" {...username} />
+        <Input label="senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
