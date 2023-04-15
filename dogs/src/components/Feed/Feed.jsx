db1c31a3b215375323e7c3e840ef41fdf2db89be
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2023-04-15 17:34:18 -0300

    feat: adição do feed e feedphotos

--- dogs/src/components/Feed/Feed.jsx
+++ dogs/src/components/Feed/Feed.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhoto from "./FeedPhotos";
 
-const Feed = () => {};
+const Feed = () => {
+  return (
+    <div>
+      <FeedModal />
+      <FeedPhoto />
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
